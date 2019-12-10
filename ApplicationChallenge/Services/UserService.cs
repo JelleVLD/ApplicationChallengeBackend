1e8b996822804eb8a6aa0d4a07fc56c99b6a92f5
Jelle VanLangendonck <jelle.vld@telenet.be>
2019-12-10 09:41:33 +0100

    gebruikerId toegevoegd aan claims

--- ApplicationChallenge/Services/UserService.cs
+++ ApplicationChallenge/Services/UserService.cs
@@ -33,6 +33,22 @@ namespace ApplicationChallenge.Services
             var userType = _apiContext.UserTypes.SingleOrDefault(x => x.Id == user.UserTypeId);
             var tokenHandler = new JwtSecurityTokenHandler();
             var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var gebruikerId="";
+            if (user.MakerId.ToString() != "")
+            {
+                gebruikerId = user.MakerId.ToString();
+            }
+            else
+            {
+                if (user.BedrijfId.ToString() != "")
+                {
+                    gebruikerId= user.BedrijfId.ToString();
+                }
+                else
+                {
+                   gebruikerId= user.AdminId.ToString();
+                }
+            }
             var tokenDescriptor = new SecurityTokenDescriptor
             {
                 Subject = new ClaimsIdentity(new Claim[]
@@ -40,9 +56,10 @@ namespace ApplicationChallenge.Services
                 new Claim("UserLoginId", user.Id.ToString()),
                 new Claim("Username", user.Username),
                 new Claim("Password", user.Password),
-                new Claim(ClaimTypes.Role, userType.Soort)
-            }
-            ),
+                new Claim(ClaimTypes.Role, userType.Soort),
+                new Claim("GebruikerId", gebruikerId) }),
+
+
                 Expires = DateTime.UtcNow.AddDays(7),
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
             };
