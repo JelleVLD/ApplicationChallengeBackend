22b519b5002a9c96bc224da13a8a2f9a21bca984
Sacha Kempeneer <sachakempeneer@msn.com>
2019-12-10 14:54:46 +0100

    prevented looping

--- ApplicationChallenge/Startup.cs
+++ ApplicationChallenge/Startup.cs
@@ -21,6 +21,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using ApplicationChallenge.Services;
 using ApplicationChallenge.Handlers;
 using Microsoft.AspNetCore.Identity;
+using Newtonsoft.Json;
 
 namespace ApplicationChallenge
 {
@@ -38,7 +39,7 @@ namespace ApplicationChallenge
         {
             services.AddCors(o => o.AddPolicy("MyPolicy", builder => { builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader(); }));
             services.AddDbContext<ApplicationContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2).AddJsonOptions(options => { options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore; }); ; ;
             services.AddTransient<IImageHandler, ImageHandler>();
             services.AddTransient<IImageWriter,ImageWriter>();
             var appSettingsSection = Configuration.GetSection("AppSettings");
