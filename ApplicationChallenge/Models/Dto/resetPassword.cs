00172f6e97d4fbee3bc1329e84572c763f380822
DinVanwezemael <37238893+DinVanwezemael@users.noreply.github.com>
2019-12-14 19:45:11 +0100

    tags maker gefixt

---
+++ ApplicationChallenge/Models/Dto/resetPassword.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApplicationChallenge.Models.Dto
+{
+    public class resetPassword
+    {
+        public string email;
+    }
+}
---
+++ ApplicationChallenge/Models/Mail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApplicationChallenge.Models
+{
+    [NotMapped]
+    public class Mail
+    {
+        public long EmailID { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
